6b3d11597fa223d1c4e4477b53cad2a546ff2838
lucas <lucas_sbn@outlook.com>
2024-09-09 12:03:54 -0300

    Implementação da API para utilizar o Whatsapp

---
+++ app.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const wppconnect = require('@wppconnect-team/wppconnect');
+
+wppconnect
+  .create({
+    session: 'sessionName',
+    catchQR: (base64Qr, asciiQR) => {
+      console.log(asciiQR); // Optional to log the QR in the terminal
+      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+      if (matches.length !== 3) {
+        return new Error('Invalid input string');
+      }
+      response.type = matches[1];
+      response.data = new Buffer.from(matches[2], 'base64');
+
+      var imageBuffer = response;
+      require('fs').writeFile(
+        'out.png',
+        imageBuffer['data'],
+        'binary',
+        function (err) {
+          if (err != null) {
+            console.log(err);
+          }
+        }
+      );
+    },
+    logQR: false,
+  })
+  .then((client) => start(client))
+  .catch((error) => console.log(error));
